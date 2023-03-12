31228905c8345c9b1de3655dbea09f727b89ea03
guillermo <h.uhm292@gmail.com>
2023-03-12 22:26:30 +0100

    first commit

---
+++ tests/FizzBuzz.test.js
@@ -0,0 +1,55 @@
+import { fizzBuzz } from "../src/scripts/FizzBuzz";
+
+describe('FizzBuzz', () => {
+    test('should return Fizz', () => {
+        let number = 3
+        let result = fizzBuzz(number)
+        expect(result).toBe("Fiz")
+        
+    });
+  
+});
+
+describe('FizzBuzz', () => {
+    test('should return Buzz', () => {
+        let number = 3
+        let result = fizzBuzz(number)
+        expect(result).toBe("Buz")
+     
+    });
+
+    });
+
+    describe('FizzBuzz', () => {
+        test('should return Fizz-Buzz', () => {
+            let number = (3 , 5) 
+            let result = fizzBuzz(number)
+            expect(result).toBe("FizzBuzz")
+         
+        });
+    
+        });
+
+        describe('FizzBuzz', () => {
+            test('should return Fizz-Buzz', () => {
+                let number = (3 , 5) 
+                let result = fizzBuzz(number)
+                expect(result).toBe("FizzBuzz")
+             
+         });
+        
+         });
+
+         describe('FizzBuzz', () => {
+            test('should return Fizz-Buzz', () => {
+                let number = number 
+                let result = fizzBuzz(number)
+                expect(result).toBe("numbe")
+             
+         });
+        
+         });
+    
+
+
+
