013f13f6524cf0083b7a4c4c276c282fcee0241a
guillermo <h.uhm292@gmail.com>
2023-03-14 19:48:34 +0100

    finish kata fizz-buzz js

--- src/scripts/FizzBuzz.js
+++ src/scripts/FizzBuzz.js
@@ -1,13 +1,18 @@
-export function fizzBuzz(number){   
-if(number % 3 === 0 && number % 5 !== 0){
-    return "Fizz"
-}
- if(number % 5 === 0 && number % 3 !== 0){
-    return "Buzz"
-}
- if(number % 3 === 0 && number % 5 === 0){
-    return "Fizz-Buzz"
-}
- return number
+export function fizzBuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "Fizz-Buzz"
+    }
     
+    if (number % 3 === 0) {
+        return "Fizz"
+    }
+    if (number % 5 === 0) {
+        return "Buzz"
+    }
+    if (number % 3 === 0) {
+        return "Fizz-Buzz"
+    }
+    return number
+    
+
 }
